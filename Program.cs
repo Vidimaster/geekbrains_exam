dc46e073bee6d5a18fbf9e86a09e1b4c5e7f5bbf
Vidimaster <vidimaster@yandex.com>
2023-06-19 17:55:04 +0700

    Task solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] input)
+{
+    System.Console.Write("[\"" + string.Join("\", \"", input) + "\"]");
+}
+
+string[] CheckForLength(string[] input)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= 3)
+        {
+            count++;
+        }
+
+    }
+
+    string[] result = new string[count];
+
+    count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= 3)
+        {
+            result[count] = input[i];
+            count++;
+        }
+
+    }
+    return result;
+
+}
+
+string[] input = new string[6];
+
+input[0] = "Hi";
+input[1] = "12121";
+input[2] = "2";
+input[3] = "Hiwewe";
+input[4] = "1ee";
+input[5] = "ff";
+
+PrintArray(input);
+
+System.Console.Write(" -> ");
+
+PrintArray(CheckForLength(input));
+
+System.Console.WriteLine();
